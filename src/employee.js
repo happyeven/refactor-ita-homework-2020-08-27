521eceb45cb0b2aa05d5dbff82309f8a653fbbcc
Dong <1361840595@qq.com>
2020-08-28 21:55:24 +0800

    [refactor] [dong] refactor: delete the useless code

--- src/employee.js
+++ src/employee.js
@@ -1,20 +1,8 @@
 class Employee {
   constructor(name, type) {
-    this.validateType(type);
     this._name = name;
     this._type = type;
   }
-
-  validateType(type) {
-    if (![
-      'engineer',
-      'manager',
-      'salesman',
-    ].includes(type)) {
-      throw new Error(`Employee cannot be of type ${type}`);
-    }
-  }
-
   toString() {
     return `${this._name} (${this._type})`;
   }

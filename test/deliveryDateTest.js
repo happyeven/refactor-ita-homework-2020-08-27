db78b8deef39ec0e73b977e5a6cb2c8d08643264
Dong <1361840595@qq.com>
2020-08-28 22:08:35 +0800

    [refactor] [dong] test : add should return 3 when deliveryDate given isRush true and deliveryState NY

--- test/deliveryDateTest.js
+++ test/deliveryDateTest.js
@@ -13,3 +13,16 @@ deliveryDateTest('should return 2 when deliveryDate given isRush true and delive
     const result = deliveryDate(anOrder,isRush);
     t.is(2, result)
 });
+deliveryDateTest('should return 3 when deliveryDate given isRush true and deliveryState NY', t => {
+    let isRush = true
+    let anOrder = {
+        deliveryState: 'NY',
+        placedOn: {
+            plusDays: function (data) {
+                return data;
+            }
+        }
+    }
+    const result = deliveryDate(anOrder,isRush);
+    t.is(3, result)
+});

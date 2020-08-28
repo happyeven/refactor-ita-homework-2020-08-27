22648f1f651ceba403e7deaf5bf966bd2a159b97
Dong <1361840595@qq.com>
2020-08-28 21:39:34 +0800

    [refactor] [dong] test: add should return error when toString given a type dong

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,9 +1,18 @@
 const employeeTest = require('ava');
 const { Employee } = require('../src/employee.js');
  
-employeeTest('should return correct message when toString given emplyee engineer', t => {
+employeeTest('should return correct message when toString given employee engineer', t => {
     let employee = new Employee("Dong", "engineer");
     let result = employee.toString();
     t.is("Dong (engineer)", result);
+});
+
+employeeTest('should return error when toString given a type dong', t => {
+    try {
+        let employee = new Employee("", "Dong");
+        t.fail();
+    } catch (e) {
+        t.is("Employee cannot be of type Dong", e.message);
+    }
 })
  

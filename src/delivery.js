7ab50c013876ac642960661917de0a4f6831dcb6
happyeven <1361840595@qq.com>
2020-08-29 01:03:52 +0800

    [refactor] [dong] refactor : extract calculateDateIdRushFalse

--- src/delivery.js
+++ src/delivery.js
@@ -3,30 +3,34 @@ function deliveryDate (anOrder, isRush) {
     return calculateDateIsRushTrue(anOrder);
   }
   else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return calculateDateIdRushFalse(anOrder);
   }
 }
 
 module.exports = {
   deliveryDate
 };
+function calculateDateIdRushFalse(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  }
+  else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
 function calculateDateIsRushTrue(anOrder) {
   let deliveryTime;
   if ([
